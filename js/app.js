527322d1cda5f8d7b2a8e21588b5e49313b3d87d
Draquix <Draquix@yahoo.com>
2021-02-10 20:05:19 -0600

    ok got pagination, each page populates select with its keywords, select shows only those with keyword, and also theres 3 radio buttons that show only stuff with the specified amount of horns for the page.

--- js/app.js
+++ js/app.js
@@ -27,43 +27,54 @@ $.ajax('data/page-1.json').then(callStuffBack => {
         $('select').append(`<option value="${horner.keyword}">${horner.keyword}</option>`);
         console.log('json horners:',horner);
     });
-
-    
-    $('select').change( function () {
-        const choice = $('select').find(':selected').text();
-        console.log(choice);
-        $('div').hide();
-        $(`.${choice}`).show();
-    });
-    horns.forEach(horner => { horner.render();});
+    showPage(1);
 })
 
 $.ajax('data/page-2.json').then(callStuffBack => {
         callStuffBack.forEach( (horner) => {
         horns2.push(new Horn(horner.image_url,horner.title,horner.description,horner.keyword,horner.horns));
-        $('select').append(`<option value="${horner.keyword}">${horner.keyword}</option>`);
     });
 
-    
-    $('select').change( function () {
-        const choice = $('select').find(':selected').text();
-        console.log(choice);
-        $('div').hide();
-        $(`.${choice}`).show();
-    });
-    
 })
+    
+$('select').change( function () {
+    const choice = $('select').find(':selected').text();
+    $('section').empty();
+    if ( pageNumber === 1) {
+        horns.forEach(horner => {
+            if (horner.keyword === choice) {
+                horner.render();
+            }
+        });
+    }
+    if ( pageNumber === 2) {
+        horns2.forEach(horner => {
+            if (horner.keyword === choice) {
+                horner.render();
+            }
+        });
+    }
+});
+
 function showPage(page) {
     console.log(`page ${page} clicked`);
     if ( page === 1 ) {
         pageNumber = 1;
         $('section').empty();
-        horns.forEach(horner => { horner.render();});
+        $('select').empty();
+        horns.forEach(horner => {
+            horner.render();
+            $('select').append(`<option value="${horner.keyword}">${horner.keyword}</option>`);
+        });
     }
     if ( page === 2 ) {
         pageNumber = 2;
         $('section').empty();
-        horns2.forEach(horner => { horner.render();});
+        $('select').empty();
+        horns2.forEach(horner => { 
+            horner.render();
+            $('select').append(`<option value="${horner.keyword}">${horner.keyword}</option>`);
+        });
     }
 }
 
